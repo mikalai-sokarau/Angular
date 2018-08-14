35b031e1a0dc49f928ac827d50afb2cbbf1cf0f6
Mikalai Sokarau <mikalaisokarau@gmail.com>
2018-08-14 12:02:42 +0300

    added course logic

--- src/app/components/course/course.component.ts
+++ src/app/components/course/course.component.ts
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./course.component.scss']
 })
 export class CourseComponent implements OnInit {
+  name = 'course name';
+  duration = '1h 39m';
+  date = '01.01.1980';
+  description = 'course description';
 
   constructor() { }
 
