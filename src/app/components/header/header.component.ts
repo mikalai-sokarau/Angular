0fc8016616064fe59f9d10db487d7c2fbb925875
Mikalai Sokarau <mikalaisokarau@gmail.com>
2018-08-14 17:31:45 +0300

    changed login button behavior

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   login = 'user';
+  logInButtonText = this.login ? "Log out" : "Log in";
 
   constructor() { }
 
