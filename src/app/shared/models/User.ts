41f27616bd83b11dd6e8e3825d813b42856cd553
Mikalai Sokarau <mikalaisokarau@gmail.com>
2018-08-15 10:15:54 +0300

    added user class

---
+++ src/app/shared/models/User.ts
@@ -0,0 +1,5 @@
+import { UserInterface } from './user-interface';
+
+export default class implements UserInterface {
+  constructor(public id: string, public firstName: string, public lastName: string) {}
+}
